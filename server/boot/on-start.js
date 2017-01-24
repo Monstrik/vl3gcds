e1eff262189c223683708f59df2dc1ae349aa3e8
AlexYakovis <yakovis@boylesoftware.com>
2017-01-24 15:12:40 -0500

    add native model on start action

---
+++ server/boot/on-start.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+module.exports = function (app) {
+  app.on('started', function () {
+    // app.models.Account.destroyAll();
+    // setInterval(function () {
+    //   console.log('iam here!');
+    // }, 3000);
+  })
+};
